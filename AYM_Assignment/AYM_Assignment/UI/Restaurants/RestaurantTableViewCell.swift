6a4b649e2920515911c992c9e170709a8f5325b1
Mohammad Allam <mohammada.allam@gmail.com>
2018-06-02 07:33:27 +0400

    Creating and adding the connections between the RestaurantVC and it's viewModel

---
+++ AYM_Assignment/AYM_Assignment/UI/Restaurants/RestaurantTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantTableViewCell.swift
+//  AYM_Assignment
+//
+//  Created by Mohammad Allam on 6/2/18.
+//  Copyright © 2018 Allam. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AYM_Assignment/AYM_Assignment/UI/Restaurants/RestaurantViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RestaurantViewModel.swift
+//  AYM_Assignment
+//
+//  Created by Mohammad Allam on 6/2/18.
+//  Copyright © 2018 Allam. All rights reserved.
+//
+
+import Foundation
